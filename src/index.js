476967dc50a36eaa0e5d9871020160fccce1938a
st33ze <st33ze@protonmail.com>
2024-11-19 16:33:57 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import pizzaPng from  './assets/pizza.png';
+
+// const pizzaImg = document.createElement('img');
+// pizzaImg.classList.add('pizzaImg');
+// pizzaImg.src = pizzaPng;
+// pizzaImg.alt = 'pizza';
+// document.body.appendChild(pizzaImg);
