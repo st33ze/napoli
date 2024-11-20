acff43f8e4b75ce4a937a5944b99ecda3802a594
st33ze <st33ze@protonmail.com>
2024-11-20 11:59:51 +0100

    style: initial style template

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import './styles.css';
-import pizzaPng from  './assets/pizza.png';
-
-// const pizzaImg = document.createElement('img');
-// pizzaImg.classList.add('pizzaImg');
-// pizzaImg.src = pizzaPng;
-// pizzaImg.alt = 'pizza';
-// document.body.appendChild(pizzaImg);
