2b7560fd6a2df05077973bcd6e59d9ace92273c5
st33ze <st33ze@protonmail.com>
2024-11-22 14:19:53 +0100

    refactor(header): calculate nav height on mobile

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.getElementById('header');
   const nav = header.querySelector('nav');
-  const menu = header.querySelector('.hamburger-menu');
+  const hamburgerMenu = header.querySelector('.hamburger-menu');
   const overlay = document.querySelector('.overlay');
-  menu.addEventListener('click', () => {
-    menu.classList.toggle('opened');
-    nav.classList.toggle('opened');
+  let isAnimating = false;
+  hamburgerMenu.addEventListener('click', () => {
+    if (isAnimating) return;
+    isAnimating = true;
+    hamburgerMenu.classList.toggle('open');
     overlay.classList.toggle('active');
-  })
+    toggleNav(nav);
+    setTimeout(() => {
+      isAnimating = false;
+    }, 500);
+  });
 });
+
+function toggleNav(nav) {
+  const navHeight = nav.offsetHeight;
+  if (navHeight === 0) nav.style.height = `${nav.scrollHeight}px`;
+  else nav.style.height = 0;
+}
