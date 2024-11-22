ff135650ff4ed7986a727055977a17af8b11ed96
st33ze <st33ze@protonmail.com>
2024-11-22 11:28:58 +0100

    feat: init main with overlay

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const header = document.getElementById('header');
   const nav = header.querySelector('nav');
   const menu = header.querySelector('.hamburger-menu');
+  const overlay = document.querySelector('.overlay');
   menu.addEventListener('click', () => {
     menu.classList.toggle('opened');
     nav.classList.toggle('opened');
+    overlay.classList.toggle('active');
   })
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import './styles.css';
 
+
 /* ====================|| Header ||==================== */
 import header from './components/header/header.html';
 import './components/header/header.css';
 import './components/header/header.js';
 document.body.insertAdjacentHTML('beforeend', header);
+
+/* ====================|| Main ||==================== */
+import main from './components/main/main.html';
+import './components/main/main.css';
+import './components/main/main.js';
+document.body.insertAdjacentHTML('beforeend', main);
