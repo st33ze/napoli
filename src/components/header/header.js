0f5073ef9898538bd88477c6131ca8ba0795689e
st33ze <st33ze@protonmail.com>
2024-11-28 15:30:55 +0100

    refactor(header): module structure
    - Change module export to dom element instead of function
    - Remove not needed classes
    - Reorganized toggleNav() for better reusability

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,39 +1,42 @@
 import headerHTML from './header.html';
 import './header.css';
 
-export function loadHeader(parent) {
-  parent.insertAdjacentHTML('beforeend', headerHTML);
 
-  const header = document.getElementById('header');
-  const nav = header.querySelector('nav');
-  const hamburger = header.querySelector('.hamburger-menu');
-  let isAnimating = false;
-  hamburger.addEventListener('click', () => {
-    if (isAnimating) return;
-    isAnimating = true;
-    
-    const event = new CustomEvent('toggleMenu');
-    document.dispatchEvent(event);
-    
-    const isOpen = hamburger.classList.toggle('open');
-    toggleNav(nav);
-    
-    // Update aria attributes for accessibility
-    hamburger.setAttribute('aria-expanded', isOpen);
-    nav.setAttribute('aria-hidden', !isOpen);
-    
-    setTimeout(() => {
-      isAnimating = false;
-    }, 500);
-  });
-  
-  nav.addEventListener('click', (e) => {
-    document.dispatchEvent(new CustomEvent('navClick', {detail: e.target.dataset.link}));
-  });
-}
+const container = document.createElement('div');
+container.innerHTML = headerHTML;
 
-function toggleNav(nav) {
+const header = container.firstChild;
+const nav = header.querySelector('nav');
+const hamburger = header.querySelector('.hamburger-menu');
+
+let isAnimating = false;
+function toggleNav() {
+  if (isAnimating) return;
+  isAnimating = true;
+
+  const event = new CustomEvent('toggleMenu');
+  document.dispatchEvent(event);
+
+  const isOpen = hamburger.classList.toggle('open');
   const navHeight = nav.offsetHeight;
   if (navHeight === 0) nav.style.height = `${nav.scrollHeight}px`;
   else nav.style.height = 0;
+
+  // Update aria attributes for accessibility
+  hamburger.setAttribute('aria-expanded', isOpen);
+  nav.setAttribute('aria-hidden', !isOpen);
+
+  setTimeout(() => {
+    isAnimating = false;
+  }, 600);
 }
+
+hamburger.addEventListener('click', toggleNav);
+
+nav.addEventListener('click', (e) => {
+  if (e.target.dataset.link) toggleNav();
+  document.dispatchEvent(new CustomEvent('navClick', {detail: e.target.dataset.link}));
+});
+
+
+export { header };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './styles.css';
 
-import { loadHeader } from './components/header/header.js';
-import { loadMain } from './components/main/main.js';
+// import { loadHeader } from './components/header/header.js';
+// import { loadMain } from './components/main/main.js';
+import { header } from './components/header/header.js';
 
 const app = document.createElement('div');
 app.id = 'app';
 document.body.appendChild(app);
 
-loadHeader(app);
-loadMain(app);
+// loadHeader(app);
+// loadMain(app);
+
+app.appendChild(header);
